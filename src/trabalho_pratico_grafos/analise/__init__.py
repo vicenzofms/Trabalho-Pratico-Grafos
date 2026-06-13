2f0bcb16bed90361100d9ef5d902556b3e71ad61
Vicenzo Fonseca <vicenzofms@gmail.com>
2026-06-13 18:38:59 -0300

    fix: arremates gerais e docstring modularidade melhorada

---
+++ src/trabalho_pratico_grafos/analise/__init__.py
@@ -0,0 +1,4 @@
+from .comunidades import modularidade, louvain
+from .pontes import arestas_intercomunidade, pontes_classicas, pontes_locais
+
+__all__ = ["modularidade", "louvain", "arestas_intercomunidade", "pontes_classicas", "pontes_locais"]
--- src/trabalho_pratico_grafos/minerador/minerador.py
+++ src/trabalho_pratico_grafos/minerador/minerador.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, asdict
-from typing import TypedDict, Literal
-from threading import Thread, Lock
+from typing import Literal
+from threading import Lock
 from concurrent.futures import ThreadPoolExecutor, Future
 import json
 import time
